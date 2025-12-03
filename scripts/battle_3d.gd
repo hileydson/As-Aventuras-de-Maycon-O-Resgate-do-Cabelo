b51d19ddc83caeeb666fcc49a520a36ef3ef2af8
Hileydson Luiz Cogo <hileydson@hotmail.com>
2025-12-03 17:51:18 -0300

    Nova tela de batalha

---
+++ scripts/battle_3d.gd
@@ -0,0 +1,16 @@
+extends Camera3D
+
+#@onready var camera_3d: Camera3D = $"."
+#@onready var maycon: AnimatedSprite3D = $"../Maycon"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	##maycon.play("float")
+	#camera_3d.
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
