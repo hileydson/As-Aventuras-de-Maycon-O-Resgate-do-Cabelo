773662939937fa118cf7436a8d0bf1956561ec7a
Hileydson Luiz Cogo <hileydson@hotmail.com>
2025-12-07 18:54:28 -0300

    Battle refinamento - ajuste camera

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4b33b6ff0396f7af9e34699f1150a374fd53471")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac09da21d5bb00435860a712bfd21c118441f6c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
