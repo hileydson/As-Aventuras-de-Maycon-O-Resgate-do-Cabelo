60c705de7f664f83d898f8818c698ff530a09bd5
hileydson <hileydson@hotmail.com>
2025-12-17 22:30:00 -0300

    Tamanho do maycon redefinido na batalha

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96dbdd35c877adc55533933622f33ca2750260b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edaea1dfe460e1c909216aeff4fc15330f1bcf8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
