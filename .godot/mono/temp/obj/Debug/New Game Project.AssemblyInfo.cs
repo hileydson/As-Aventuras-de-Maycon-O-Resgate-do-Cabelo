c4cc63628512503e217db5953f0ddc287f61dbb1
hileydson <hileydson@hotmail.com>
2025-12-17 23:44:21 -0300

    Ajustado até o before castle 3

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cb257318dfeb6fb6e4174efe8f6387948fcb251")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e93e00595600373708c86707b5785d64166d9fba")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
