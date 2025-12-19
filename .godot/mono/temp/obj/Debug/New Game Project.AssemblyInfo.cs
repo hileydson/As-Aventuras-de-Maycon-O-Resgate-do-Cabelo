ef84a005829db786a3c505f19b1795de5a030e50
hileydson <hileydson@hotmail.com>
2025-12-19 14:45:03 -0300

    Bomba Pretti funcional

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23ce02b015227444cf78351c03a3a4a7d21a29db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10eaf3bcec577f253337a3d19c4d17a8aebac461")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
