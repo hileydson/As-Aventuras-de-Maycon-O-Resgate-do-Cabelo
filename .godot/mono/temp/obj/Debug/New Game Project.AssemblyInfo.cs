905218d0e38038268705a41ba80ac13c3d1213d6
hileydson <hileydson@hotmail.com>
2025-12-26 20:57:06 -0300

    Inimigo Manga Completo

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3ddf0f78567a8155b8a55cbf2ca74c3b6384b78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706402bcbb19b62bcff9bb7697fa0ee7528e8a21")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
