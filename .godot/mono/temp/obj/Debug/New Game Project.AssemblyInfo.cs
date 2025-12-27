391382763528fa2df0dd346ba99fca67d4887a87
hileydson <hileydson@hotmail.com>
2025-12-26 21:11:32 -0300

    AJustado

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706402bcbb19b62bcff9bb7697fa0ee7528e8a21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a6dc649f2c4bc6254b99316f14a6da49d23aafe")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
