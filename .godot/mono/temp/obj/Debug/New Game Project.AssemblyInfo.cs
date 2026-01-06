0ce3302cde497e5702c505b446408b2af37d54d8
hileydson <hileydson@hotmail.com>
2026-01-06 15:35:22 -0300

    Mais ajustes na introducao

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32a0fa790b9ede577d5bd79211992eb255ea8ffa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99712302c76f146b42e8b31faea9f0cb2e97d35a")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
