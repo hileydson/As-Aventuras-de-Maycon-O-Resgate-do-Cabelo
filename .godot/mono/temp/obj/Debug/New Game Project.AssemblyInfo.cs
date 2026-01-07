99a6c7c12036b6787cec43602786e9d24c397f3b
hileydson <hileydson@hotmail.com>
2026-01-07 14:51:08 -0300

    Add android partial support and fixes

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6974ecbf84954a3cc4bd37390a0b486e6a801e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+785a79c29b547acaf71070d6a29a10a6e7533e2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
