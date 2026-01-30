8fa5b4f1534f756a007418bb4dea006dc964be16
hileydson <hileydson@hotmail.com>
2026-01-30 13:32:51 -0300

    Time pacoca logo

--- scripts/intro_pacoca_producoes.gd
+++ scripts/intro_pacoca_producoes.gd
@@ -1,7 +1,14 @@
 extends VideoStreamPlayer
+@onready var to_hide: Node2D = $".."
 
 func _on_finished() -> void:
-	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	pass #get_tree().change_scene_to_file("res://scenes/menu.tscn")
 
 func _process(delta):
 	pass
+
+
+func _on_node_2d_ready() -> void:
+	await get_tree().create_timer(12.9).timeout
+	#to_hide.visible = false
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
