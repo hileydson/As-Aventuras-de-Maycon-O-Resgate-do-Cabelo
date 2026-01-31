b40dbd782c148e19fdfc879976b714e0b5625aa1
hileydson <hileydson@hotmail.com>
2026-01-31 13:11:27 -0300

    Gun load

--- scripts/3D/bullets.gd
+++ scripts/3D/bullets.gd
@@ -1,5 +1,6 @@
 extends Area3D
 @onready var gun_load: AudioStreamPlayer = $"../GunLoad"
+@onready var me: Area3D = $"."
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,7 +10,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Global.maycon_pegou_arma_first_3d_battle:
+		me.visible = true
+	else:
+		me.visible = false
 
 
 func _on_body_entered(body: Node3D) -> void:
