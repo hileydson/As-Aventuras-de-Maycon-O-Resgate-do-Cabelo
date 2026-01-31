eb58365a638080b5fae95f2226dc1e93e7843cac
hileydson <hileydson@hotmail.com>
2026-01-31 13:46:13 -0300

    Maycon 3d died first battle

--- scripts/3D/character_body_3d.gd
+++ scripts/3D/character_body_3d.gd
@@ -6,16 +6,23 @@ extends CharacterBody3D
 @export var JOY_SENSITIVITY = 0.05 # Sensibilidade para o controle
 @onready var walk: AudioStreamPlayer2D = $"../walk"
 @onready var jump: AudioStreamPlayer2D = $"../jump"
-@onready var arma_canvas: CanvasLayer = $arma_canvas
-@onready var arma_sprite: AnimatedSprite2D = $arma_canvas/Control/arma_sprite
+@onready var hud_canvas: CanvasLayer = $hud_canvas
+@onready var arma_sprite: AnimatedSprite2D = $hud_canvas/control_gun/arma_sprite
 @onready var gun_load: AudioStreamPlayer = $"../GunLoad"
 @onready var gun_shot: AudioStreamPlayer = $"../GunShot"
-@onready var shoot_fire: AnimatedSprite2D = $arma_canvas/Control/shoot
-@onready var balas_numero: Label = $arma_canvas/Control/balas_numero
+@onready var shoot_fire: AnimatedSprite2D = $hud_canvas/control_gun/shoot
+@onready var balas_numero: Label = $hud_canvas/control_gun/balas_numero
+@onready var control_gun: Control = $hud_canvas/control_gun
+@onready var camera_3d: Camera3D = $Camera3D
+@onready var hurt_sound_3d: AudioStreamPlayer = $"../../HurtSound3d"
 
 @onready var camera = $Camera3D
 
+var shake_intensity = 0.0
+var shake_decay = 5.0 # Quão rápido a tremedeira para
+
 var gun_bullets_count=0
+var danos_count = 0
 
 func add_bullets_to_gun(number:int):
 	gun_bullets_count = gun_bullets_count+number
@@ -34,8 +41,31 @@ func _unhandled_input(event):
 		camera.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 
+func aplicar_shake(valor: float):
+	shake_intensity = valor
+
 func _physics_process(delta):
 	
+	# HUD de sangue
+	$hud_canvas/maycon_hp/hp_1.visible = Global.maycon_danos_first_3d_battle<=4
+	$hud_canvas/maycon_hp/hp_2.visible = Global.maycon_danos_first_3d_battle<=3
+	$hud_canvas/maycon_hp/hp_3.visible = Global.maycon_danos_first_3d_battle<=2
+	$hud_canvas/maycon_hp/hp_4.visible = Global.maycon_danos_first_3d_battle<=1
+	$hud_canvas/maycon_hp/hp_5.visible = Global.maycon_danos_first_3d_battle<=0
+	
+	#acabou de levar um dano
+	if Global.maycon_danos_first_3d_battle != danos_count:
+		danos_count = Global.maycon_danos_first_3d_battle
+		aplicar_shake(0.4)
+		hurt_sound_3d.play()
+		
+		
+	# YOU DIED
+	if Global.maycon_danos_first_3d_battle == 5:
+		#get_tree().paused = true
+		#$".".process_mode = Node.PROCESS_MODE_DISABLED
+		pass
+			
 	#se pegou bala nova soma na contagem
 	if Global.maycon_pegou_bullet:
 		arma_sprite.play("reload")
@@ -45,6 +75,18 @@ func _physics_process(delta):
 	#CONTA BALAS
 	balas_numero.text = "X "+str(gun_bullets_count)
 	
+	#tela tremer
+	if shake_intensity > 0:
+		# Escolhe uma direção aleatória baseada na intensidade
+		camera_3d.h_offset = randf_range(-1, 1) * shake_intensity
+		camera_3d.v_offset = randf_range(-1, 1) * shake_intensity
+		
+		# Faz a intensidade diminuir com o tempo
+		shake_intensity = lerp(shake_intensity, 0.0, shake_decay * delta)
+	else:
+		camera_3d.h_offset = 0
+		camera_3d.v_offset = 0
+	
 	# faz o tiro
 	if Global.maycon_pegou_arma_first_3d_battle && Input.is_action_just_pressed("tiro") && arma_sprite.animation!="shoot" && gun_bullets_count!=0:
 		shoot_fire.play("shoot")
@@ -53,11 +95,11 @@ func _physics_process(delta):
 		remove_bullets_from_gun()
 	
 	if Global.maycon_pegou_arma_first_3d_battle:
-		if arma_canvas.visible == false:
+		if control_gun.visible == false:
 			arma_sprite.play("reload")
-		arma_canvas.visible = true
+		control_gun.visible = true
 	else:
-		arma_canvas.visible = false
+		control_gun.visible = false
 	
 	# --- LÓGICA DO ANALÓGICO DIREITO ---
 	var joy_look = Input.get_vector("look_left", "look_right", "look_up", "look_down")
--- scripts/3D/world_3d.gd
+++ scripts/3D/world_3d.gd
@@ -4,6 +4,9 @@ extends Node3D
 @onready var gun_load: AudioStreamPlayer = $GunLoad
 @onready var seco_welcome: AudioStreamPlayer = $SecoWelcome
 @onready var maycon_3d: Node3D = $maycon_3d
+@onready var you_died: Label = $you_died
+@onready var fire_seco_3d: Node3D = $fire_seco_3d
+@onready var fade: Node2D = $fade
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,6 +20,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	# YOU DIED
+	if Global.maycon_danos_first_3d_battle == 5:
+		#get_tree().paused = true
+		you_died.visible = true
+		fire_seco_3d.process_mode = Node.PROCESS_MODE_DISABLED
+		maycon_3d.process_mode = Node.PROCESS_MODE_DISABLED
+		await get_tree().create_timer(3.0).timeout 
+		fade.get_node("Transition").play("fade_out")
+		await get_tree().create_timer(2.0).timeout 
+		get_tree().change_scene_to_file("res://scenes/fase_1_before_castle_4.tscn") 
+	
 	print("DANOS MAYCON: -> "+str(Global.maycon_danos_first_3d_battle))
 	print("DANOS SECO: -> "+str(Global.seco_danos_first_3d_battle))
 	
