e318b46271de407d2c4339d612b4f6cd6f0d3b14
hileydson <hileydson@hotmail.com>
2026-02-02 16:05:00 -0300

    Fall fix

--- scripts/3D/cenario_3d_bofore_castle_1.gd
+++ scripts/3D/cenario_3d_bofore_castle_1.gd
@@ -1,6 +1,14 @@
 extends Node3D
+@onready var sangue_fill_effect: AudioStreamPlayer = $SangueFillEffect
+@onready var sliding: AudioStreamPlayer = $Sliding
 
 
 
 func _ready() -> void:
 	get_tree().get_first_node_in_group("player").get_node("hud_canvas").visible = false
+	
+	sliding.play()
+	await get_tree().create_timer(2.1).timeout
+	sangue_fill_effect.play()
+	sliding.stop()
+	get_tree().get_first_node_in_group("player").aplicar_shake(0.9)
