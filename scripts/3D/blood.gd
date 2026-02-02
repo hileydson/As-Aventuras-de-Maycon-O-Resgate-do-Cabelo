be40051f3fc6d0a3f5bb7291df9a29add3678c28
hileydson <hileydson@hotmail.com>
2026-02-02 10:26:44 -0300

    Seco 3d battle ready

--- scripts/3D/blood.gd
+++ scripts/3D/blood.gd
@@ -4,5 +4,5 @@ func _ready():
 	emitting = true # Começa o espirro
 	print("Sangue instanciado em: ", global_position)
 	# Espera 2 segundos (tempo suficiente para subir e cair) e deleta
-	await get_tree().create_timer(3.0).timeout
+	await get_tree().create_timer(2.0).timeout
 	queue_free()
