a078bb0243d0cd7ec3b8071c682eb53a744ffe45
hileydson <hileydson@hotmail.com>
2026-02-02 11:00:00 -0300

    Removed comment

--- scripts/3D/blood.gd
+++ scripts/3D/blood.gd
@@ -2,7 +2,5 @@ extends GPUParticles3D
 
 func _ready():
 	emitting = true # Começa o espirro
-	print("Sangue instanciado em: ", global_position)
-	# Espera 2 segundos (tempo suficiente para subir e cair) e deleta
 	await get_tree().create_timer(2.0).timeout
 	queue_free()
