920a9f80468d58392354bcc55dd012f9b92b5a62
hileydson <hileydson@hotmail.com>
2026-02-11 09:53:07 -0300

    cutscene

--- scripts/3D/last_part_before_end.gd
+++ scripts/3D/last_part_before_end.gd
@@ -2,17 +2,15 @@ extends Node3D
 @onready var maycon_3d: Node3D = $maycon_3d
 @onready var fade: Node2D = $fade
 @onready var cidade_perdida: Label = $cidade_perdida
+@onready var cutscene_inicio: AnimationPlayer = $cutscene/cutscene_inicio
+@onready var the_almost_end_song: AudioStreamPlayer = $TheAlmostEndSong
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#seta final do game para pegar equipar a moto
-	get_tree().get_first_node_in_group("player").set_final_game()
-	
-	await get_tree().create_timer(2.0).timeout 
-	cidade_perdida.visible = true
-	await get_tree().create_timer(3.0).timeout 
-	cidade_perdida.visible = false
+	# cutscene
+	get_tree().get_first_node_in_group("player").get_node("hud_canvas").get_node("maycon_hp").visible = false
+	cutscene_inicio.play("intro_mapa")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,3 +29,13 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		fade.get_node("Transition").play("fade_out")
 		await get_tree().create_timer(2.0).timeout 
 		get_tree().change_scene_to_file("res://scenes/demo_end.tscn")
+
+
+func _on_cutscene_inicio_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "intro_mapa":
+		#seta final do game para pegar equipar a moto
+		fade.get_node("Transition").play("fade_in")
+		var player = get_tree().get_first_node_in_group("player")
+		player.set_final_game()
+		player.get_node("hud_canvas").get_node("maycon_hp").visible = true
+		the_almost_end_song.play()
