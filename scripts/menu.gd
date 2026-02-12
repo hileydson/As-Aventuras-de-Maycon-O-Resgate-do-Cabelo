664c8093f251af020dc1ef10ee4db9cccc2e58da
hileydson <hileydson@hotmail.com>
2026-02-12 14:35:11 -0300

    VERSION EDITION 1.2

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,10 +1,14 @@
 extends Control
 
+@onready var version: Label = $Camera2D/version
 @export var load_from_castle_1:bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.load_from_castle_1 = load_from_castle_1
+	
+	version.text = "v"+ProjectSettings.get_setting("application/config/version")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
